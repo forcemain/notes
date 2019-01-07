6118ffa70037255b1bcac12f0b9417e7d57beebc
李满满 <limanman@corp.netease.com>
2019-01-07 15:13:19 +0800

    :underage:

--- Django/基础教程/入门相关/投票应用.codes/mysite/polls/urls.py
+++ Django/基础教程/入门相关/投票应用.codes/mysite/polls/urls.py
@@ -14,8 +14,8 @@ app_name = 'polls'
 
 
 urlpatterns = [
-    url('^$', views.question.index, name='tpl-poll-index'),
-    url('(?P<question_pk>[0-9]+)/$', views.question.detail, name='tpl-poll-detail'),
-    url('(?P<question_pk>[0-9]+)/vote/$', views.question.vote, name='rdr-poll-vote'),
-    url('(?P<question_pk>[0-9]+)/result/$', views.question.result, name='tpl-poll-result')
+    url('^$', views.question.PollIndexView.as_view(), name='tpl-poll-index'),
+    url('(?P<question_pk>[0-9]+)/$', views.question.PollDetailView.as_view(), name='tpl-poll-detail'),
+    url('(?P<question_pk>[0-9]+)/vote/$', views.question.PollVoteView.as_view(), name='rdr-poll-vote'),
+    url('(?P<question_pk>[0-9]+)/result/$', views.question.PollResultView.as_view(), name='tpl-poll-result')
 ]
--- Django/基础教程/入门相关/投票应用.codes/mysite/polls/views/question.py
+++ Django/基础教程/入门相关/投票应用.codes/mysite/polls/views/question.py
@@ -8,51 +8,52 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import ListView, DetailView, View
 
 
 from .. import models
 
 
-def index(request):
-    page = request.GET.get('page', 1)
+class PollIndexView(ListView):
+    model = models.Question
+    context_object_name = 'questions'
+    paginate_by = settings.DEFAULT_PAGE_SIZE
+    queryset = models.Question.objects.all()
 
-    queryset = models.Question.objects.order_by('-pub_date')
-    paginator = Paginator(queryset, per_page=settings.DEFAULT_PAGE_SIZE)
-    try:
-        questions = paginator.page(page)
-    except PageNotAnInteger:
-        questions = paginator.page(1)
-    except EmptyPage:
-        questions = paginator.page(paginator.num_pages)
-
-    return render(request, 'polls/index.html', {'questions': questions})
+    template_name = 'polls/index.html'
 
 
-def detail(request, question_pk):
-    question = get_object_or_404(models.Question, pk=question_pk)
+class PollDetailView(DetailView):
+    model = models.Question
+    pk_url_kwarg = 'question_pk'
+    context_object_name = 'question'
+    queryset = models.Question.objects.all()
 
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def result(request, question_pk):
-    question = get_object_or_404(models.Question, pk=question_pk)
-
-    return render(request, 'polls/result.html', {'question': question})
+    template_name = 'polls/detail.html'
 
 
-def vote(request, question_pk):
-    question = get_object_or_404(models.Question, pk=question_pk)
-    try:
-        choice = question.choice_set.get(pk=request.POST['choice_pk'])
-    except (KeyError, models.Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': 'no choice selected'
-        })
-    else:
-        choice.votes += 1
-        choice.save()
+class PollResultView(DetailView):
+    model = models.Question
+    pk_url_kwarg = 'question_pk'
+    context_object_name = 'question'
+    queryset = models.Question.objects.all()
 
-        redirect_url = reverse_lazy('polls:tpl-poll-result', args=(question.pk,))
-        return HttpResponseRedirect(redirect_url)
+    template_name = 'polls/result.html'
+
+
+class PollVoteView(View):
+    def post(self, request, question_pk):
+        question = get_object_or_404(models.Question, pk=question_pk)
+        try:
+            choice = question.choice_set.get(pk=request.POST['choice_pk'])
+        except (KeyError, models.Choice.DoesNotExist):
+            return render(request, 'polls/detail.html', {
+                'question': question,
+                'error_message': 'no choice selected'
+            })
+        else:
+            choice.votes += 1
+            choice.save()
+
+            redirect_url = reverse_lazy('polls:tpl-poll-result', args=(question.pk,))
+            return HttpResponseRedirect(redirect_url)
